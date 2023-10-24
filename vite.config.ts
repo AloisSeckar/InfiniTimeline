cb746abdca273fcadc5dfb4229978d02a72f83d6
Alois Sečkár <ellrohir@seznam.cz>
2023-10-24 21:28:44 +0200

    feat: inject CSS in build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
 
 // https://vitejs.dev/config/
 // https://blog.egmond.dev/vue-component-to-npm-package
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(), 
+    libInjectCss()
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'lib/main.ts'),
